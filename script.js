4983834a12d11a2c6322d1858bdde0f8b2160ec0
Binay Mann <82080713+Binay-tech1608@users.noreply.github.com>
2026-06-11 02:33:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function volume_sphere() {
     //Write your code here
-  
+    let rad = document.getElementById("radius");
+	let r =  ParseInt(rad.value);
+	let volumeConstant = 4.1887902047863905;
+	let Volume = volumeConstant * Math.pow(r,3);
+	let v = document.getElementById("volume");
+	v.textContent = Volume;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
